eb7b35fd9a9eae6cb8cabd3c36ddc1da9b8cdf30
Ashutosh Pratap Mall <ashupratap.26@gmail.com>
2026-08-06 15:46:25 +0530

    feat: add Prisma authentication

--- apps/api/src/routes/auth.routes.ts
+++ apps/api/src/routes/auth.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 const authRouter = express.Router();
-import { register } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 
 authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 
 export { authRouter };
