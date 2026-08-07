1a0cdc232518173c139325768b90a9f2722914eb
Ashutosh Pratap Mall <ashupratap.26@gmail.com>
2026-08-07 18:25:33 +0530

    Add GitHub profile synchronization and authentication

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,6 +3,8 @@ import {Request, Response} from "express";
 import { githubRouter } from "./routes/github.routes.js";
 import cors from "cors";
 import { authRouter } from "./routes/auth.routes.js";
+import { profileRouter } from "./routes/profile.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({
@@ -10,9 +12,10 @@ app.use(cors({
   credentials:true
 }))
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/github", githubRouter);
-
+app.use("/api/profile", profileRouter);
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
